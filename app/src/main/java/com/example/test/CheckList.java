c451f663697ceaa0ccaa3374d76ba4d26890d5b2
hyeju <20211403@sungshin.ac.kr>
2022-10-10 19:13:05 +0900

    git commit --amend

---
+++ app/src/main/java/com/example/test/CheckList.java
@@ -0,0 +1,34 @@
+package com.example.test;
+
+public class CheckList {
+    private String profile;
+    private String title;
+    private String subtitle;
+
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public CheckList(){}
+}
---
+++ app/src/main/java/com/example/test/CheckListView.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_list_view);
+    }
+}
