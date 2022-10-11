6ce45a3553aae9529af405fc33b21eaea7002162
hyeju <20211403@sungshin.ac.kr>
2022-10-11 18:11:11 +0900

    git commit --amend

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
